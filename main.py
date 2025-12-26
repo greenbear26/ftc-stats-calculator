291df89d522ec9d0c4c1a4e41ff1d6bb9d9e986d
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2025-12-26 16:11:24 -0500

    Added partner and match diff statistics

--- main.py
+++ main.py
@@ -13,15 +13,16 @@ def main(eventCode, season, min_team_index):
     matches_matrix = np.zeros((len(matches)*2, len(teams)))
     offense_scores = np.zeros(len(matches)*2)
     defense_scores = np.zeros(len(matches)*2)
-    team_opponents = {team: [] for team in teams}
+    team_opponents = {team: set() for team in teams}
+    team_partners = {team: set() for team in teams}
     for match_index, match in enumerate(matches):
         red_score = match['scores']['red']['totalPointsNp']
         blue_score = match['scores']['blue']['totalPointsNp']
 
-        red_teams = [team['teamNumber'] for team in match['teams'] if
-                     team['alliance'] == 'Red']
-        blue_teams = [team['teamNumber'] for team in match['teams'] if
-                      team['alliance'] == 'Blue']
+        red_teams = set([team['teamNumber'] for team in match['teams'] if
+                     team['alliance'] == 'Red'])
+        blue_teams = set([team['teamNumber'] for team in match['teams'] if
+                      team['alliance'] == 'Blue'])
 
         for team in match['teams']:
             team_number = team['teamNumber']
@@ -30,12 +31,14 @@ def main(eventCode, season, min_team_index):
                 matches_matrix[match_index*2][team_index] = 1
                 offense_scores[match_index*2] = red_score
                 defense_scores[match_index*2] = blue_score
-                team_opponents[team_number].extend(blue_teams)
+                team_opponents[team_number].update(blue_teams)
+                team_partners[team_number].update(red_teams - {team_number})
             else:
                 matches_matrix[match_index*2+1][team_index] = 1
                 offense_scores[match_index*2+1] = blue_score
                 defense_scores[match_index*2+1] = red_score
-                team_opponents[team_number].extend(red_teams)
+                team_opponents[team_number].update(red_teams)
+                team_partners[team_number].update(blue_teams - {team_number})
 
     # OPR/DPR/CCWM Calculation
     opr = np.linalg.lstsq(matches_matrix, offense_scores, rcond=None)[0]
@@ -48,17 +51,31 @@ def main(eventCode, season, min_team_index):
     avg_opponents_opr = []
     avg_opponents_dpr = []
     avg_opponents_ccwm = []
+    avg_partners_opr = []
+    avg_partners_dpr = []
+    avg_partners_ccwm = []
     for team in teams:
         opponents = team_opponents[team]
         if len(opponents) == 0:
             avg_opponents_opr.append(0)
             avg_opponents_dpr.append(0)
             avg_opponents_ccwm.append(0)
-            continue
-        opponent_indices = [teams.index(opponent) for opponent in opponents]
-        avg_opponents_opr.append(np.round(np.mean(opr[opponent_indices]), 2))
-        avg_opponents_dpr.append(np.round(np.mean(dpr[opponent_indices]), 2))
-        avg_opponents_ccwm.append(np.round(np.mean(ccwm[opponent_indices]), 2))
+        else:
+            opponent_indices = [teams.index(opponent) for opponent in opponents]
+            avg_opponents_opr.append(np.round(np.mean(opr[opponent_indices]), 2))
+            avg_opponents_dpr.append(np.round(np.mean(dpr[opponent_indices]), 2))
+            avg_opponents_ccwm.append(np.round(np.mean(ccwm[opponent_indices]), 2))
+
+        partners = team_partners[team]
+        if len(partners) == 0:
+            avg_partners_opr.append(0)
+            avg_partners_dpr.append(0)
+            avg_partners_ccwm.append(0)
+        else:
+            partner_indices = [teams.index(partner) for partner in partners]
+            avg_partners_opr.append(np.round(np.mean(opr[partner_indices]), 2))
+            avg_partners_dpr.append(np.round(np.mean(dpr[partner_indices]), 2))
+            avg_partners_ccwm.append(np.round(np.mean(ccwm[partner_indices]), 2))
 
     # Data Presentation
     pd.set_option('display.max_rows', None)
@@ -66,31 +83,41 @@ def main(eventCode, season, min_team_index):
     team_frame = pd.DataFrame({
         'Team': teams,
         'OPR': opr,
-        'DPR': dpr,
-        'CCWM': ccwm,
+        # 'DPR': dpr,
+        # 'CCWM': ccwm,
         'Avg Opp OPR': avg_opponents_opr,
-        'Avg Opp DPR': avg_opponents_dpr,
-        'Avg Opp CCWM': avg_opponents_ccwm
+        # 'Avg Opp DPR': avg_opponents_dpr,
+        # 'Avg Opp CCWM': avg_opponents_ccwm,
+        'Avg Partner OPR': avg_partners_opr,
+        # 'Avg Partner DPR': avg_partners_dpr,
+        # 'Avg Partner CCWM': avg_partners_ccwm
     })
+
+    team_frame['Match OPR Diff'] = 2*team_frame['Avg Opp OPR'] \
+        - team_frame['Avg Partner OPR']
+    # team_frame['Match DPR Diff'] = 2*team_frame['Avg Opp DPR'] \
+    #     - team_frame['Avg Partner DPR']
+    # team_frame['Match CCWM Diff'] = 2*team_frame['Avg Opp CCWM'] \
+    #     - team_frame['Avg Partner CCWM']
 
     no_show_teams = [team for team in teams if
                      len(team_opponents[team]) == 0]
     team_frame = team_frame[~team_frame['Team'].isin(no_show_teams)]
 
     # Sort and Print
-    print(team_frame.sort_values(by='CCWM', ascending=False,
+    print(team_frame.sort_values(by='Match OPR Diff', ascending=False,
                                  ignore_index=True))
     # Event Averages
-    print(f"\nEvent Average OPR: {team_frame['OPR'].mean():.2f}")
+    # print(f"\nEvent Average OPR: {team_frame['OPR'].mean():.2f}")
 
-    # Graph
-    max_data = team_frame[team_frame.index >= min_team_index].head(10)
+    # # Graph
+    # max_data = team_frame[team_frame.index >= min_team_index].head(10)
 
-    ax = max_data.plot(x='Team', y=team_frame.columns[1:], kind='bar')
+    # ax = max_data.plot(x='Team', y=team_frame.columns[1:], kind='bar')
 
-    ax.title.set_text(f'Team Stats for Event {eventCode} ({season})')
+    # ax.title.set_text(f'Team Stats for Event {eventCode} ({season})')
 
-    plt.show()
+    # plt.show()
 
 
 
